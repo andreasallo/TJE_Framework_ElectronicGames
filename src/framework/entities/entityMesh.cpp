881641242c69217547215cc5830f51c6b7d24a0b
andrea <andrea.sallo01@estudiant.upf.edu>
2025-11-03 01:35:25 +0100

    mapa,optimitzacio culling,skybox - classe 05/11

--- src/framework/entities/entityMesh.cpp
+++ src/framework/entities/entityMesh.cpp
@@ -15,11 +15,13 @@ EntityMesh::EntityMesh(Mesh* mesh, const Material& material, const std::string& 
 
 void EntityMesh::render(Camera* camera) {
 
+	//not mesh-> dont rendering anything
     if (!mesh) {
 		Entity::render(camera);
         return;
     }
     
+	//START OF ENTITYMESH RENDER
     Shader* shader = material.shader;
 
     if (!shader) {
@@ -28,8 +30,31 @@ void EntityMesh::render(Camera* camera) {
     }
         
     float distance = 10.0f;
+	std::vector<Matrix44> visible_models; //INSTANCING VISIBLE MODELS
 
+	//CULLING INSTANCED MESHES
     if (isInstanced) {
+        for (const auto& model_matrix : models) {
+
+			//Bounding box in world space
+            BoundingBox box = transformBoundingBox(model_matrix, mesh->box);
+            const Vector3& center = box.center;
+
+            // distance culling
+            if (culling && camera->eye.distance(center) > distance) {
+                continue;
+            }
+
+            // Frustrum Culling USE SPHERE
+            if (culling && camera->testSphereInFrustum(center, box.halfsize.length()) != CLIP_INSIDE) {
+                continue; 
+            }
+
+            visible_models.push_back(model_matrix);
+        }
+        if (visible_models.empty()) {
+            return; // No hay instancias visibles, no renderizamos nada
+		}
     }
     else {
 
@@ -37,46 +62,37 @@ void EntityMesh::render(Camera* camera) {
         BoundingBox box = transformBoundingBox(globalMatrix, mesh->box);
         const Vector3& center = box.center;
 
-        if (this->culling) {
+        if (culling) {
             //distance culling
             if (camera->eye.distance(center) > distance) {
                 return;
             }
 
-            //Frustrum Culling HACEMOS LA ESFERA
+            //Frustrum Culling 
             if (camera->testSphereInFrustum(center, box.halfsize.length()) != CLIP_INSIDE) { //clip_insnide es que esta dentro del frustrum(cono d vision)
                 return;
             }
         }
        
 
-    }
-
-    //if ->else //culling for instanced meshes
-
-    
-
-    
+    } 
 
     // Set OpenGL flags
     glDisable(GL_BLEND);
     glEnable(GL_DEPTH_TEST);
     glDisable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
 
-    
-
-    // Enable shader and pass uniforms 
-	
+ 
+    //Enable shader and pass uniforms --- SHADER PART
     shader->enable();
 
 	//upload uniforms
-    shader->setUniform("u_model", model);
     shader->setUniform("u_viewprojection", camera->viewprojection_matrix);
     shader->setUniform("u_color", material.color);
     shader->setUniform("u_camera_position", camera->eye);
-    //shader->setTexture("u_texture", material.diffuse, 0); //difusse es textura
 
-    if (material.diffuse) { //si mi malla tiene MTL
+    if (material.diffuse) { //si mi malla tiene MTL ---- TEXTURE 
         shader->setTexture("u_texture", material.diffuse, 0); //difusse es textura
     }
     else {
@@ -86,14 +102,22 @@ void EntityMesh::render(Camera* camera) {
         shader->setUniform("u_texture", 0);
     }
 
-    // Render the mesh using the active shader
-    mesh->render(GL_TRIANGLES);
+	if (isInstanced) { //----------RENDER INSTANCED MESHES
+		//renderizar totes les instancies visibles
+		shader->setMatrix44Array("u_models", visible_models.data(),(int)visible_models.size()); 
+		mesh->renderInstanced(GL_TRIANGLES, visible_models.data(),(int)visible_models.size());
+	}
+        
+    else {
+        //Render single mesh
+		shader->setUniform("u_model", model);
+        mesh->render(GL_TRIANGLES);
+	}
 
     // Disable shader after finishing rendering
     shader->disable();
 
 	//END OF ENTITYMESH RENDER
-
 	Entity::render(camera);
 };
 
